337f1576ae287386f47655aaa66dc3765cd8ee9c
CreativeGP <malicktafaminecraft@gmail.com>
2018-11-29 23:45:02 +0900

    Fix compile error

--- main.c
+++ main.c
@@ -19,10 +19,14 @@
 #define LSBIT(x)   ((x)&0b00000001)
 
 typedef uint8_t u8;
-typedef int8_t i8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int8_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 /*
   :memory usage:
